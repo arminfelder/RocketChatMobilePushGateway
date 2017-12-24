964b3df8e89f5b70dbdc645978f606ac312e9975
Armin <armin.felder@osalliance.com>
2017-12-25 00:11:22 +0100

    sad

--- handlers/GooglePushHandler.cpp
+++ handlers/GooglePushHandler.cpp
@@ -1,6 +1,22 @@
-//
-// Created by armin on 16.11.17.
-//
+/********************************************************************************************************************
+ *                                                                                                                  *
+ * Copyright (C) 2017 Armin Felder                                                                                  *
+ * This file is part of RocketChatMobilePushGateway <https://git.fairkom.net/chat/RocketChatMobilePushGateway>.     *
+ *                                                                                                                  *
+ * RocketChatMobilePushGateway is free software: you can redistribute it and/or modify                              *
+ * it under the terms of the GNU General Public License as published by                                             *
+ * the Free Software Foundation, either version 3 of the License, or                                                *
+ * (at your option) any later version.                                                                              *
+ *                                                                                                                  *
+ * RocketChatMobilePushGateway is distributed in the hope that it will be useful,                                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                    *
+ * GNU General Public License for more details.                                                                     *
+ *                                                                                                                  *
+ * You should have received a copy of the GNU General Public License                                                *
+ * along with RocketChatMobilePushGateway. If not, see <http://www.gnu.org/licenses/>.                              *
+ *                                                                                                                  *
+ ********************************************************************************************************************/
 
 #include "GooglePushHandler.h"
 #include "../models/GooglePushModel.h"
@@ -11,7 +27,7 @@ void GooglePushHandler::onRequest(std::unique_ptr<HTTPMessage> headers) noexcept
 
 void GooglePushHandler::onBody(std::unique_ptr<folly::IOBuf> body) noexcept {
     if(mBody){
-        mBody->prependChain(std::move(body));
+        (*mBody).appendChain(std::move(body));
     } else{
         mBody = std::move(body);
     }
@@ -24,7 +40,7 @@ void GooglePushHandler::onUpgrade(proxygen::UpgradeProtocol prot) noexcept {
 void GooglePushHandler::onEOM() noexcept {
     if (mBody) {
         try {
-            std::string body(reinterpret_cast<const char *>(mBody->data()));
+            std::string body(reinterpret_cast<const char *>((*mBody).data()));
             GooglePushModel googlePushModel(body);
             std::string response;
             if (googlePushModel.sendMessage()) {
--- models/GooglePushModel.h
+++ models/GooglePushModel.h
@@ -1,6 +1,22 @@
-//
-// Created by armin on 16.11.17.
-//
+/********************************************************************************************************************
+ *                                                                                                                  *
+ * Copyright (C) 2017 Armin Felder                                                                                  *
+ * This file is part of RocketChatMobilePushGateway <https://git.fairkom.net/chat/RocketChatMobilePushGateway>.     *
+ *                                                                                                                  *
+ * RocketChatMobilePushGateway is free software: you can redistribute it and/or modify                              *
+ * it under the terms of the GNU General Public License as published by                                             *
+ * the Free Software Foundation, either version 3 of the License, or                                                *
+ * (at your option) any later version.                                                                              *
+ *                                                                                                                  *
+ * RocketChatMobilePushGateway is distributed in the hope that it will be useful,                                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                    *
+ * GNU General Public License for more details.                                                                     *
+ *                                                                                                                  *
+ * You should have received a copy of the GNU General Public License                                                *
+ * along with RocketChatMobilePushGateway. If not, see <http://www.gnu.org/licenses/>.                              *
+ *                                                                                                                  *
+ ********************************************************************************************************************/
 
 #ifndef ROCKETCHATMOBILEPUSHGATEWAY_GOOGLEPUSHMODEL_H
 #define ROCKETCHATMOBILEPUSHGATEWAY_GOOGLEPUSHMODEL_H
